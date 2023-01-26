0dbf1d74c7d6d8cd1a3208f6ad648910bf15e54b
권도은 <kounduk9119t@naver.com>
2023-01-26 18:56:14 +0900

    doeun1

---
+++ src/component/TodoList.js
@@ -0,0 +1,15 @@
+import TodoListItem from "./TodoListItem";
+import '../layout/TodoList.css'
+
+const TodoList = ({todos}) => {
+
+    return (
+        <div className="TodoList">
+            {todos.map(todo => (
+                <TodoListItem todo={todo} key={todo.id}/>
+            ))}
+        </div>
+    );
+};
+
+export default TodoList;
