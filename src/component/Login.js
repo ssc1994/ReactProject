f67a1462f99ea2591199d9d773d9922203db5ded
sschoi1994 <sschoi1994@naver.com>
2023-01-26 20:36:45 +0900

    final

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,23 @@
 import { Fragment } from "react"
 
 const login = () => {
-    return(
+
+    const onClickLogin = () => {
+
+    }
+
+    return (
         <Fragment>
-         <h3>로그인</h3>
+            <form action="#">
+                <h1>Sign in</h1>
+                <span>use your account</span>
+                
+                <input type="email" placeholder="Your Email"></input>
+                <input type="password" placeholder="Your Password"></input>
+
+                <a href="#">Forgot your password?</a>
+                <button onClick={onClickLogin}>Sign In</button>
+            </form>
         </Fragment>
     )
 }
