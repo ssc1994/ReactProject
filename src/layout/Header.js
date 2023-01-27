60ee738cad8a6b9ba63c9de028e101f9bc802d55
whalien207 <whalien207@gmail.com>
2023-01-27 15:31:25 +0900

    로그인 세션 키 수정 & 로그인 카테고리 로그인상태에 따라 변경
    1. 로그인 session key "login"으로 변경
    2. 로그인시 LOGOUT으로 변경하고 JOIN 안보이도록 수정

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import styled from './Header.module.css';
 
@@ -5,9 +6,34 @@ import styled from './Header.module.css';
 const Header = () => {
 
     const navigator = useNavigate();
-
     const sinclClick = () => {
         navigator("/");
+    }
+
+    const[loginCate, setLoginCate] = useState('login');
+    const[visible, setVisible] = useState(true);
+    const loginInfo = sessionStorage.getItem('login');
+
+    //mount이후에 session의 유무에 따라서 카테고리명 LOGIN / LOGOUT 변경처리
+    //& JOIN 카테고리 visible true/false 처리하여 LOGOUT일 경우에는 숨김 처리 해주었다.
+    useEffect(() => {
+        if(loginInfo !== null){
+            setLoginCate("LOGOUT");
+            setVisible(false);
+        }else{
+            setLoginCate("LOGIN");
+            setVisible(true);
+        }
+    }, [loginInfo]);
+    
+    // LOGOUT 클릭시 session 삭제 처리
+    const onClickLogCate = () => {
+        if(loginCate === "LOGOUT"){
+            sessionStorage.removeItem('login');
+            navigator('/');
+        }else{ //로그인
+            navigator('/login');
+        }
     }
 
 
@@ -23,12 +49,12 @@ const Header = () => {
                         <li><Link to="/map" className={styled.map}>MAP</Link></li>
                         <li><Link to="/record" className={styled.record}>RECORD</Link></li>
                         <li><Link to="/mypage" className={styled.mypage}>MYPAGE</Link></li>
-                        <li><Link to="/login" className={styled.login}>LOGIN</Link></li>
-                        <li><Link to="/join" className={styled.join}>JOIN</Link></li>
+                        <li><Link to="/login" className={styled.login} onClick={onClickLogCate}>{loginCate}</Link></li>
+                        {visible ? <li><Link to="/join" className={styled.join} >JOIN</Link></li> : null}
                     </ul>
                 </div>
             </nav>
-            <Outlet />
+            <Outlet/>
         </header>
     )
 }
