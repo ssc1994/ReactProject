3a1b6e3941df6af56efe476472736c274f621c56
권도은 <kounduk9119t@naver.com>
2023-01-27 12:29:16 +0900

    도으니가 커밋함

--- src/component/Mypage.js
+++ src/component/Mypage.js
@@ -1,108 +1,119 @@
-import { Fragment, useCallback, useRef, useState } from "react"
-import { Calendar } from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
-import moment from "moment";
-import TodoTemplate from "./TodoTemplate";
-import TodoInsert from "./TodoInsert";
-import TodoList from "./TodoList";
+import { useState } from 'react';
 
-//css파일 경로 링크
-import '../layout/MyPage.module.css'
-import '../layout/TodoTemplate.css'
+function Article(props) {
+    return <article>
+        <h2>{props.title}</h2>
+        {props.body}
+    </article>
+}
+function Header(props) {
+    return <header>
+        <h1><a href="/" onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+        }}>{props.title}</a></h1>
+    </header>
+}
+function Nav(props) {
+    const lis = []
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(<li key={t.id}>
+            <a id={t.id} href={'/read/' + t.id} onClick={event => {
+                event.preventDefault();
+                props.onChangeMode(Number(event.target.id));
+            }}>{t.title}</a>
+        </li>)
+    }
+    return <nav>
+        <ol>
+            {lis}
+        </ol>
+    </nav>
+}
 
-
-
-const Mypage = () => {
-    const [value, onChange] = useState(new Date()); //캘린더
-    const [todos, setTodos] = useState ([
-        {
-            id: 1,
-            text: '리액트 기초',
-            checked: true,
-        },
-        {
-            id:2,
-            text: '컴포넌트 스타일링',
-            checked: true,
-        },
-        {
-            id: 3,
-            text: '일정 관리 앱 만들어보기',
-            checked: false,
-        },
+function Update(props) {
+    const [title, setTitle] = useState(props.title);
+    const [body, setBody] = useState(props.body);
+    return <article>
+        <h2>Update</h2>
+        <form onSubmit={event => {
+            event.preventDefault();
+            const title = event.target.title.value;
+            const body = event.target.body.value;
+            props.onUpdate(title, body);
+        }}>
+            <p><input type="text" name="title" placeholder="title" value={title} onChange={event => {
+                setTitle(event.target.value);
+            }} /></p>
+            <p><textarea name="body" placeholder="body" value={body} onChange={event => {
+                setBody(event.target.value);
+            }}></textarea></p>
+            <p><input type="submit" value="Update"></input></p>
+        </form>
+    </article>
+}
+function Mypage() {
+    const [mode, setMode] = useState('WELCOME');
+    const [id, setId] = useState(null);
+    const [nextId, setNextId] = useState(4);
+    const [topics, setTopics] = useState([
+        { id: 1, title: '나루토', body: '나뭇잎마을 호카게' },
     ]);
-
-    //고유값으로 사용될 id
-    // ref를 사용하여 변수 담기
-    const nextId = useRef(4);
-
-    const onInsert = useCallback(
-        text => {
-            const todo = {
-                id: nextId.current,
-                text,
-                checked: false,
-            };
-            setTodos(todos.concat(todo));
-            nextId.current += 1; //nextId 1씩 더하기
-        },
-        [todos],
-    );
-    
-
-    //하이라이트가 들어갈 날짜를 배열로 작성했는데 안먹는이유가..
-    const marks = [
-        "15-01-2023",
-        "15-01-2023",
-    ];
-
+    let content = null;
+    let contextControl = null;
+    if (mode === 'WELCOME') {
+        content = <Article title="Welcome" body="Hello!"></Article>
+    } else if (mode === 'READ') {
+        let title, body = null;
+        for (let i = 0; i < topics.length; i++) {
+            if (topics[i].id === id) {
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+        content = <Article title={title} body={body}></Article>
+        contextControl = <li><a href={'/update/' + id} onClick={event => {
+            event.preventDefault();
+            setMode('UPDATE');
+        }}>Update</a></li>
+    } else if (mode === 'UPDATE') {
+        let title, body = null;
+        for (let i = 0; i < topics.length; i++) {
+            if (topics[i].id === id) {
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+        content = <Update title={title} body={body} onUpdate={(title, body) => {
+            console.log(title, body);
+            const newTopics = [...topics]
+            const updatedTopic = { id: id, title: title, body: body }
+            for (let i = 0; i < newTopics.length; i++) {
+                if (newTopics[i].id === id) {
+                    newTopics[i] = updatedTopic;
+                    break;
+                }
+            }
+            setTopics(newTopics);
+            setMode('READ');
+        }}></Update>
+    }
     return (
-
-        <Fragment>
-            <h3>마이페이지</h3>
-            <div>
-                {/* 달력 */}
-                <Calendar
-                    onChange={onChange}
-                    value={value}
-                    locale="en-EN"
-                    //date와 우리가 작성한 배열인 marks를 비교하며 해당되는 날짜에 highlight를 클래스명으로 주게 된다.
-                    tileClassName={({ date, view }) => {
-                        if (marks.find((x) => x === moment(date).format("DD-MM-YYYY"))) {
-                            return "highlight";
-                        }
-                    }}
-                />
-                {/* 여기까지 달력 */}
-                <br />
-                {/* todo */}
-                <TodoTemplate>
-                    <TodoInsert/>
-                    <TodoList todos={todos}/>
-                </TodoTemplate>
-
-
-
-                {/* 마이페이지 */}
-                <h3>정보수정</h3>
-                <div>
-                아이디 : <input type="text" /> <br/>
-                현재 비밀번호 : <input type="text" /><br/>
-                새 비밀번호 : <input type="text" /><br/>
-                새 비밀번호 확인: <input type="text" /><br/>
-                <button type="submit">수정완료</button>
-                </div>
-
-
-
-
-
-
-
-
-            </div>
-        </Fragment>
-    )
+        <div>
+            <Header title="회원정보 수정" onChangeMode={() => {
+                setMode('WELCOME');
+            }}></Header>
+            <Nav topics={topics} onChangeMode={(_id) => {
+                setMode('READ');
+                setId(_id);
+            }}></Nav>
+            {content}
+            <ul>
+                {contextControl}
+            </ul>
+        </div>
+    );
 }
 
 export default Mypage;
